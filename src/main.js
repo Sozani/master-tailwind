cd65888e49674f4799375d0105e098312b61362d
suzan <sys612603@gmail.com>
2022-08-13 20:35:02 +0200

    ha

--- src/main.js
+++ src/main.js
@@ -4,5 +4,23 @@ const closeBtn = document.getElementById("close");
 
 
 
+openBtn.addEventListener("click",()=>{
+    menuList.classList.add("flex");
+    menuList.classList.remove("hidden");
+    console.log('bravo suz');
+
+openBtn.classList.add("hidden");
+closeBtn.classList.remove("hidden");
+})
+closeBtn.addEventListener("click",()=>{
+    menuList.classList.add("hidden");
+    menuList.classList.remove("flex");
+    
+    closeBtn.classList.add("hidden");
+    console.log('bravo');
+    openBtn.classList.remove("hidden");
+})
+
+
 
 
